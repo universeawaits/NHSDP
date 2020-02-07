fb990b74e126509ac8f87847b33bb32fd4531679
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-02-07 12:29:53 +0300

    Improve studying place create view

--- NHSDP_Request_handling/NHSDP_Request_handling.WEB/Controllers/InternshipController.cs
+++ NHSDP_Request_handling/NHSDP_Request_handling.WEB/Controllers/InternshipController.cs
@@ -1,10 +1,9 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Mvc;
+
 using NHSDP_Request_handling.Core.Model;
 using NHSDP_Request_handling.Logic.Interface;
 using NHSDP_Request_handling.WEB.ViewModel;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
 
 namespace NHSDP_Request_handling.WEB.Controllers
 {
--- NHSDP_Request_handling/NHSDP_Request_handling.WEB/Controllers/StudyingPlaceController.cs
+++ NHSDP_Request_handling/NHSDP_Request_handling.WEB/Controllers/StudyingPlaceController.cs
@@ -1,12 +1,15 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+
 using NHSDP_Request_handling.Core.Model;
 using NHSDP_Request_handling.Logic.Interface;
 using NHSDP_Request_handling.WEB.ViewModel;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace NHSDP_Request_handling.WEB.Controllers
 {
@@ -27,19 +30,20 @@ namespace NHSDP_Request_handling.WEB.Controllers
         public override async Task<IActionResult> CreateView()
         {
             IEnumerable<Office> offices = await officeService.GetAllAsync();
-            ViewData["Offices"] = mapper.Map<IEnumerable<OfficeVM>>(offices).Select(x =>
+            ViewData["Offices"] = mapper.Map<IEnumerable<OfficeVM>>(offices).Select(o =>
                                     new SelectListItem()
                                     {
-                                        Text = x.Adress,
-                                        Value = x.Id.ToString()
+                                        Text = o.Adress,
+                                        Value = o.Id.ToString()
                                     }).ToList();
 
             IEnumerable<Internship> internships = await internshipService.GetAllAsync();
-            ViewData["Internships"] = mapper.Map<IEnumerable<InternshipVM>>(internships).Select(x =>
+            ViewData["Internships"] = mapper.Map<IEnumerable<InternshipVM>>(internships).Select(i =>
                                         new SelectListItem()
                                         {
-                                            Text = x.StartAt.Date.ToString() + " " + x.EnrollmentState,
-                                            Value = x.Id.ToString(),
+                                            Text = i.StartAt.Date.ToShortDateString() + "-" + i.EndAt.Date.ToShortDateString() 
+                                                + ", " + i.EnrollmentState,
+                                            Value = i.Id.ToString(),
                                         }).ToList();
 
             return await base.CreateView();
