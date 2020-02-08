ef21e7b1130cb40300bf6cdb2e21dcc401266eec
River Endless <myowngollum@gmail.com>
2020-02-08 10:40:06 +0300

    Add logging

---
+++ NHSDP_Request_handling/NHSDP_Request_handling.WEB/Logging/ConsoleLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace NHSDP_Request_handling.WEB.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string information)
+        {
+            Console.WriteLine(information);
+        }
+    }
+}
