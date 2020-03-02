53636214a6f9b9e7c21b3fb314ff2012c35d17c5
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-03-02 14:26:29 +0300

    Add contexts

---
+++ NHSDP_SPA/NHSDP_SPA.Auth/Data/AppIdentityDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace NHSDP_SPA.Auth.Data
+{
+    public class AppIdentityDbContextFactory : DesignTimeDbContextFactoryBase<AppIdentityDbContext>
+    {
+        protected override AppIdentityDbContext CreateNewInstance(DbContextOptions<AppIdentityDbContext> options)
+        {
+            return new AppIdentityDbContext(options);
+        }
+    }
+}
