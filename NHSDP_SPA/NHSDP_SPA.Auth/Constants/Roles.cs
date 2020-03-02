e1fe467f30923dacac48bd1e3e82655fe017813f
Анжелика Стрижич <ao.stryzhych@lab.vebtech.by>
2020-03-02 14:38:06 +0300

    Add claim providing

---
+++ NHSDP_SPA/NHSDP_SPA.Auth/Constants/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NHSDP_SPA.Auth.Constants
+{
+    public static class Roles
+    {
+        public const string Consumer = "consumer";
+    }
+}
